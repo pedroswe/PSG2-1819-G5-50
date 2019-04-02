33617e151c43a881092d95586cd2a24939866f7b
Angel Delgado <angeldelgadoluna@gmail.com>
2019-04-02 17:59:35 +0200

    Cause has been completed

--- src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataCauseRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataCauseRepository.java
@@ -1,9 +1,15 @@
 package org.springframework.samples.petclinic.repository.springdatajpa;
 
+import java.util.Collection;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.samples.petclinic.model.Cause;
 import org.springframework.samples.petclinic.repository.CauseRepository;
 
-public interface SpringDataCauseRepository extends CauseRepository, Repository<Cause, Integer>{
-
+public interface SpringDataCauseRepository extends CauseRepository, Repository<Cause, Integer> {
+	
+	@Query("select c from Cause c where c.owner.id = ?1")
+	Collection<Cause> findAll(Integer ownerId) throws DataAccessException;
 }
